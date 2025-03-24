3a06d08eeeae690cae5fe7a0130b030ef7d8517e
Amarius Wilson <amariuswilsonclark@gmail.com>
2025-03-24 12:18:22 -0700

    Finished adding methods for arraylist.java

--- src/utils/ArrayList.java
+++ src/utils/ArrayList.java
@@ -21,12 +21,18 @@ public class ArrayList<E> implements List<E> {
         size = 0;
 
     }
+    private void ensureSpace(){
+        if (size == data.length){
+            data = Arrays.copyOf(data, data.length * 2);
+
+        }
+    }
 
     public ArrayList(List<E> otherList) {
         this(otherList.size());
         addAll(otherList);
     }
-
+    // method to make sure there is enough space in the array list to add more items
 
     // to add items at certain indexes
     @Override
@@ -57,56 +63,67 @@ public class ArrayList<E> implements List<E> {
 
     @Override
     public void clear() {
+        data = (E[]) new Object[STARTING_SPACE];
+        size = 0;
+
 
     }
 
     @Override
     public boolean contains(E item) {
-        return false;
+        return indexOf(item) >= 0;
     }
 
     @Override
     public E get(int index) {
-        return null;
+        if (index < 0 || index >= size) throw new IndexOutOfBoundsException();
+        return data[index];
     }
-
+    // gives me the index of whichever item
     @Override
     public int indexOf(E item) {
-        return 0;
+        for (int i = 0; i < size; i++){
+            if (data[i].equals(item)) {
+                return i;
+            }
+        }
+        return -1;
     }
 
     @Override
     public boolean isEmpty() {
-        return false;
+        return size == 0;
     }
 
     // for item removal via index
     public E remove(int index){
         if (index < 0 || index >= size) throw new IndexOutOfBoundsException();
-        E removed = data[index];
+        E removedItem = data[index];
         System.arraycopy(data, index + 1, data, index, size - index - 1);
         size--;
-        return removed;
+        return removedItem;
     }
-
+    // to remove first iteration of an item
     @Override
     public boolean remove(E item) {
-        return false;
+        int index = indexOf(item);
+        if (index == -1) return false;
+        remove(index);
+        return true;
     }
 
-    @Override
-    public boolean removeAll(List<E> otherList) {
-        return false;
-    }
 
     @Override
     public E set(int index, E item) {
-        return null;
+        if (index < 0 || index >= size) throw new IndexOutOfBoundsException();
+        E prevItem = data[index];
+        data[index] = item;
+        return prevItem;
     }
 
     @Override
     public int size() {
-        return 0;
+        return size;
     }
 
 }
